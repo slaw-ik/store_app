346f9dcc5eb8818cb83cb2b8cd5259b4e75bb0fd
slaw-ik <slaw-ik@yandex.ru>
2013-02-05 00:33:24 +0200

    Configured for deployment to Heroku

--- config/application.rb
+++ config/application.rb
@@ -60,5 +60,9 @@ module StoreApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Heroku requires this to be false
+    config.assets.initialize_on_precompile=false
+
   end
 end
